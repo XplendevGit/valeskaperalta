1f337422583fc515c85410c9414b8e4672bfdcf6
XplendevGit <l.riveraa664@hotmail.com>
2024-04-14 23:32:14 -0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      Esto se pondrá buenisimo y Muy Entretenido , tu puedes!
+    </div>
+  )
+}
