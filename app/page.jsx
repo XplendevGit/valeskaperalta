e8ed327bb27f3cda2da891392161498ef6cc3f5f
XplendevGit <l.riveraa664@hotmail.com>
2024-04-20 12:50:16 -0400

    Updates add reseñas-servicios-sobremi

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Portada from '@/components/sections/inicio/portada/Portada'
 import Beneficios from '@/components/sections/inicio/beneficios/Beneficios'
-import SobreMi from '@/components/sections/inicio/sobre-mi/Sobremi'
+import SobreMi from '@/components/sections/inicio/sobre-mi/SobreMi'
 import Reseñas from '@/components/sections/inicio/reseñas/Reseñas'
 
 export default function Home() {
