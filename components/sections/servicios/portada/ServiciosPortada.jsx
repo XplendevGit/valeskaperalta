3c1cc392c97f37add4b13515d30393b0ddea624a
XplendevGit <l.riveraa664@hotmail.com>
2024-05-03 00:22:21 -0400

    Updates add Services First Interface

--- components/sections/servicios/portada/ServiciosPortada.jsx
+++ components/sections/servicios/portada/ServiciosPortada.jsx
@@ -4,13 +4,13 @@ const ServiciosPortada = ({ services }) => {
   console.log(services)
     return (
       <>
-        <div className="overflow-hidden flex min-h-screen h-full w-full flex-col items-center justify-between lg:px-24 lg:p-y-24 px-6 py-24 bg-gradient-to-r from-[#B7A1BC] to-[#d46d7c]">
+        <div className="flex flex-col space-y-6 overflow-hidden min-h-screen h-full w-full items-center justify-between lg:px-24 lg:p-y-24 px-6 py-24 bg-gradient-to-r from-[#B7A1BC] to-[#d46d7c]">
 
        {services.map((service, index) => (
     
         <div 
         key={index}
-        className="container w-full flex h-full justify-center text-center">
+        className="flex container w-full h-full justify-center text-center">
     
           <div className="flex w-[428px] h-[628px] bg-white rounded-2xl flex-col p-6 space-y-2">
             {/* Contenedor de Membersía */}
