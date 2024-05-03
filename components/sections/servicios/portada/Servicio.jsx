d8813b7e3c67fed78b421000c349fe076277709b
XplendevGit <l.riveraa664@hotmail.com>
2024-05-03 00:19:17 -0400

    Updates

--- components/sections/servicios/portada/Servicio.jsx
+++ components/sections/servicios/portada/Servicio.jsx
@@ -7,7 +7,7 @@ import ServiciosPortada from './ServiciosPortada';
 const Servicio = () => {
     const [services, setService] = useState([
         {
-          title: "Primera Consulta Nutricional",
+          title: "Ingreso",
           description: "Primera consulta nutricional etc etc",
           price: "30.000",
           duration: "1.30 Hrs",
@@ -15,23 +15,12 @@ const Servicio = () => {
           backgroundColor: "#57E8D8"
         },
         {
-          name: "Pollo a la Naranja",
-          type: "Almuerzo",
-          eat: "1 Persona",
-          calories: "250 Calorías",
-          popularity: 3,
+          title: "Control",
+          price: "30.000",
+          duration: "1.30 Hrs",
           imageUrl: "https://i.postimg.cc/GmgJBZNs/eiliv-aceron-0g-Q8-Fh1f54k-unsplash-removebg-preview.png",
           backgroundColor: "#F2CD5D"
         },
-        {
-          name: "Receta 3",
-          type: "Almuerzo",
-          eat: "1 Persona",
-          calories: "480 Calorías",
-          popularity: 3,
-          imageUrl: "https://i.postimg.cc/hjtBCQsY/eiliv-aceron-m-W0-LIBBHNd-E-unsplash-removebg-preview.png",
-          backgroundColor: "#E5979E"
-        }
       ]);
     
 
